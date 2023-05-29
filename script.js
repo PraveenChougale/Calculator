5af32820f8842eea355ef55e413903021cacab06
PraveenChougale <130298073+PraveenChougale@users.noreply.github.com>
2023-05-29 11:48:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let userInput =  document.getElementById("user_input");
+let expression = "";
+
+press = (num) => {
+  expression += num;
+  userInput.value = expression;
+}
+
+equal = () => {
+  userInput.value = eval(expression);
+  expression = "";
+}
+
+erase = () => {
+  expression = "";
+  userInput.value = expression;
+}
